1a804b46618df83a50d9e86b4f8eb5202a8bf096
Lucas <lucas.grard@outlook.fr>
2021-05-17 23:57:28 +0200

    Create first view login

--- App.js
+++ App.js
@@ -1,21 +1,85 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import {Button, Image, ImageBackground, StyleSheet, Text, TextInput, TouchableOpacity, View} from 'react-native';
+import bg from './assets/john-arano-_qADvinJi20-unsplash.jpg';
+import logo from './assets/image1.jpeg';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+        return (
+            <View style={styles.container}>
+                {/*<ImageBackground source={bg} style={styles.bg} blurRadius={3}>*/}
+                    <Image source={logo} style={styles.logo}/>
+                    <TextInput
+                        placeholder="Email"
+                        style={styles.textInput}
+                        // onChangeText={(username) => this.setState({ username })}
+                        multiline={false}
+                    />
+                    <TextInput
+                        placeholder="Mot de passe"
+                        style={styles.textInput}
+                        marginTop={15}
+                        multiline={false}
+                        secureTextEntry={true}
+                        // onChangeText={(password) => this.setState({ password })}
+                    />
+                    <TouchableOpacity onPress={() => alert('Hello, world!')} style={styles.button}>
+                        <Text style={styles.buttonText}>CONNEXION</Text>
+                    </TouchableOpacity>
+                <TouchableOpacity onPress={() => alert('Hello, world!')} style={styles.button}>
+                    <Text style={styles.buttonText}>INSCRIPTION</Text>
+                </TouchableOpacity>
+                {/*</ImageBackground>*/}
+                <StatusBar style="auto" />
+            </View>
+        );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    bg: {
+        flex: 1,
+        resizeMode: 'cover',
+        width:'100%',
+        // justifyContent: 'center',
+        alignItems: 'center',
+    },
+    logo: {
+        borderRadius: 200 / 2,
+        width: 200,
+        height: 200,
+        marginTop: 40,
+        borderColor: 'black',
+        borderWidth: 2,
+    },
+    textInput: {
+        // flex: 1,
+        backgroundColor: '#fff',
+        borderRadius: 50 / 2,
+        width: 300,
+        height: 60,
+        marginTop: 200,
+        borderColor: 'black',
+        borderWidth: 2,
+        textAlign:'center',
+    },
+    button: {
+        backgroundColor: "#001766",
+        padding: 12,
+        borderRadius: 60 / 2,
+        marginTop: 10,
+        height:40,
+        // borderColor: 'black',
+        // borderWidth: 2,
+    },
+    buttonText: {
+        fontSize: 15,
+        fontWeight: 'bold',
+        color: '#fff',
+    },
 });
